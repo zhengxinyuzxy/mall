d0d30e4966a06240c51d92d5b96a544d570eeda5
zhengxinyu <mellowzz@163.com>
2023-08-06 20:35:27 +0800

    删除部分Knife4jConfiguration配置

--- gmall-parent/common/service-util/src/main/java/com/atguigu/gmall/common/knife4j/Knife4jConfiguration.java
+++ gmall-parent/common/service-util/src/main/java/com/atguigu/gmall/common/knife4j/Knife4jConfiguration.java
@@ -64,6 +64,7 @@ public class Knife4jConfiguration {
                         .description("参考mellow的博客")
                         .url("https://www.192.168.200.200/zhengxinyu/blog/"));
     }
+
     @Bean
     public GroupedOpenApi openAPI() {
         return GroupedOpenApi.builder()
@@ -72,95 +73,4 @@ public class Knife4jConfiguration {
                 .build();
     }
 
-    /**
-     * 创建API应用
-     * apiInfo() 增加API相关信息
-     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
-     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
-     * 多人开发可以设置多个Docket
-     */
-    /*@Bean
-    public Docket webApiConfig() {
-
-        //添加head参数start
-        List<Parameter> pars = new ArrayList<>();
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        tokenPar.name("userId")
-                .description("token-用户ID")
-                .defaultValue("1")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .required(false)
-                .build();
-        pars.add(tokenPar.build());
-
-        ParameterBuilder tmpPar = new ParameterBuilder();
-        tmpPar.name("userTempId")
-                .description("token-临时用户ID")
-                .defaultValue("1")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .required(false)
-                .build();
-        pars.add(tmpPar.build());
-        //添加head参数end
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("webApi")
-                .apiInfo(this.webApiInfo())
-                .select()
-                //过滤掉admin路径下的所有页面
-                .paths(Predicate.and(PathSelectors.regex("/api/.*")))
-//                .paths(PathSelectors.regex("/api/.*"))
-                //过滤掉所有error或error.*页面
-                //.paths(Predicates.not(PathSelectors.regex("/error.*")))
-                .build()
-                .globalOperationParameters(pars);
-
-    }
-
-    *//**
-     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
-     * 访问地址：http://项目实际地址/swagger-ui.html
-     *//*
-    private ApiInfo webApiInfo() {
-
-        return new ApiInfoBuilder()
-                .title("网站-API文档")
-                .description("本文档描述了网站微服务接口定义")
-                .version("1.0")
-                .contact(new Contact("mellow", "http://baidu.com", "mellowzz@163.com"))
-                .build();
-    }
-
-    *//**
-     * 创建API应用
-     * apiInfo() 增加API相关信息
-     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
-     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
-     * 多人开发可以设置多个Docket
-     *//*
-    @Bean
-    public Docket adminApiConfig() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("adminApi")
-                .apiInfo(this.adminApiInfo())
-                .select()
-                //只显示admin路径下的页面
-                .paths(Predicate.and(PathSelectors.regex("/admin/.*")))
-                .build();
-    }
-
-    *//**
-     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
-     * 访问地址：http://项目实际地址/swagger-ui.html
-     *//*
-    private ApiInfo adminApiInfo() {
-        return new ApiInfoBuilder()
-                .title("后台管理系统-API文档")
-                .description("本文档描述了后台管理系统微服务接口定义")
-                .version("1.0")
-                .contact(new Contact("mellow", "http://baidu.com", "mellowzz@163.com"))
-                .build();
-    }*/
 }
